3cdc14fc3f914e0bfdbfefb2a5b02a83f9fe7544
HongQuan78 <vhquan313@gmail.com>
2024-03-24 18:32:45 +0700

    longest common prefix

---
+++ LongestCommonPrefix/Longest Common Prefix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(LongestCommonPrefix(["flower", "flow", "flight"]));
+    }
+
+    static string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0) return "";
+        if(strs.Length == 1) return strs[0];
+        string result = "";
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+                {
+                    return result;
+                }
+            }
+
+            result += strs[0][i];
+        }
+        return result;
+    }
+}
